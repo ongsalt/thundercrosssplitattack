2cf47fae4bacb39f260e275f6702d2f590e153f1
Oachi <ongsa.srp.mkm@gmail.com>
2022-10-04 22:18:01 +0700

    add rickroll to fallback

--- pages/500.js
+++ pages/500.js
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router"
+
+
 function Error() {
+  const router = useRouter()
+  if (typeof window !== 'undefined') {
+    // window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank')
+    // setRickrolled(true)
+    router.push('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  }
   return (
     <div> you fucked up the server</div>
   )
 }
+
 
 export default Error
